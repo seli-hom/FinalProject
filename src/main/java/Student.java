a206bd1359379ef5454de4808a1c014aac5028b7
2057195 <efremselihom1@gmail.com>
2024-05-13 13:12:28 -0400

    Student class added to code

---
+++ src/main/java/Student.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Adress adress;
+    private Departement departement;
+    private ArrayList<Course> registeredCourse;
+    private ArrayList<Double> finalScores;
+    private static String nextId = 1;
+
+    public static boolean registerCourse(String courseId){
+
+    }
+
+    public static boolean dropCourse(String courseId){
+
+    }
+
+}
