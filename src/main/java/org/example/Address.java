8b865b462a09b04680253ad42630fee4d2df5ef6
Selihom Efrem Ogbe <2057195@edu.vaniercollege.qc.ca>
2024-05-13 13:08:03 -0400

    Initial commit Create Address class

---
+++ src/main/java/org/example/Address.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Adress {
+    private int streetNo;
+    private String street;
+    private String city;
+    private String province;
+    private String postalCode;
+    private String country;
+
+    public static boolean isPostalCodeValid(String postalCode){
+        // check if postal code is valid
+    }
+
+}
