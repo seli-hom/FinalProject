68f50c8483abf0ed2c989d5701db2f776eafc7a4
%username% <%email%>
2024-05-26 15:33:34 -0400

    Added code to unit test AssignmentTest

--- src/test/java/AssignmentTest.java
+++ src/test/java/AssignmentTest.java
@@ -1,2 +1,13 @@
+import org.example.Assignment;
+import org.junit.jupiter.api.Test;
+
 public class AssignmentTest {
+   @Test
+    public void testCalcAssignmentAvg(){
+       Assignment assignment = new Assignment();
+       assignment.setScores(Integer 90, 89, 71, 73, 90, 56);
+       double  expected = 76;
+       double result = calcAssignmentAvg(assignment);
+   }
+
 }
